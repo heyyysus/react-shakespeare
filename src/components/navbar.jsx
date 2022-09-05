8ae8077580609f2fdbc9174c7faddf1fe1745034
Glitch (glitch-hello-react) <none>
2022-09-05 01:38:22 +0000

    🍼⏱ Checkpoint ./src/styles/styles.css:47905959/180 ./src/components/navbar.jsx:47905959/347

---
+++ src/components/navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+/*
+  props: {
+    selection: number
+  }
+*/
+
+const Navbar = (props) => {
+  const numOfElements = 4;
+  const selection = props.selection % numOfElements;
+  const items = ["Home", "Plays", "Predictor", "About"];
+  
+  
+  
+}
